0887669bc2c21741842567806522ec12decd08de
ferrygun <ferrygun@yahoo.com>
2014-11-10 15:14:48 +0800

    Create BIPADL3.controller.js

---
+++ view/BIPADL3.controller.js
@@ -0,0 +1,53 @@
+sap.ui.controller("sap.ui.demo.myFiori.view.BIPADL3", {
+
+	onInit : function() {
+		var oBus = new sap.ui.getCore().getEventBus();
+        oBus.subscribe("nav", "to", this.handleFetchDetails, this);
+    },
+    
+    handleFetchDetails : function(sCannelID, sEvtId, oData) {
+        var oContext = oData.data.context;
+        if (oContext) {
+            this.getView().setBindingContext(oContext);
+        }
+    },
+
+   
+   	doNavBack: function(event) {
+		app.ref.AppView.app.back();
+    }, 
+
+	doHome: function(evt) {
+		var bus = sap.ui.getCore().getEventBus();
+		bus.publish("nav", "to", { 
+			id : "bipad_main",
+			data : {
+				//context : sO 
+			}
+		});
+	},
+
+	onNavButtonToDetail: function(evt, sO) {
+		var bindingContext = evt.getSource().getBindingContext(); 
+
+		var bus = sap.ui.getCore().getEventBus();
+		bus.publish("nav", "to", { 
+			id : "bipad_detail",
+			data : {
+				context : sO 
+			}
+		});
+	},
+
+	onNavButtonTo: function(evt, sO) {
+		var bindingContext = evt.getSource().getBindingContext(); 
+
+		var bus = sap.ui.getCore().getEventBus();
+		bus.publish("nav", "to", { 
+			id : "bipad_l4",
+			data : {
+				context : sO 
+			}
+		});
+	},
+});
