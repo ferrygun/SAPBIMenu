807a47fc82b913a3e77c4ac70d7cd4bb66fcb700
ferrygun <ferrygun@yahoo.com>
2014-11-10 15:04:03 +0800

    Create Component.js

---
+++ Component.js
@@ -0,0 +1,32 @@
+jQuery.sap.declare("sap.ui.demo.myFiori.Component");
+
+sap.ui.core.UIComponent.extend("sap.ui.demo.myFiori.Component", {
+
+	createContent : function() {
+		// create root view
+		var oView = sap.ui.view({
+			id : "app",
+			viewName : "sap.ui.demo.myFiori.view.App",
+			type : "JS",
+			viewData : { component : this }
+		});
+
+		// set i18n model
+		var i18nModel = new sap.ui.model.resource.ResourceModel({
+			bundleUrl : "res/i18n.properties"
+		});
+		oView.setModel(i18nModel, "i18n");
+
+		// set device model
+		var deviceModel = new sap.ui.model.json.JSONModel({
+			isPhone : jQuery.device.is.phone,
+			listMode : (jQuery.device.is.phone) ? "None" : "SingleSelectMaster",
+			listItemType : (jQuery.device.is.phone) ? "Active" : "Inactive"
+		});
+		deviceModel.setDefaultBindingMode("OneWay");
+		oView.setModel(deviceModel, "device");
+
+		// done
+		return oView;
+	}
+});
